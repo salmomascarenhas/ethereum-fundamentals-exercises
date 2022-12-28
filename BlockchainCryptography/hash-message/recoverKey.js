d226aace47ef42225f9367d3613d133d0dacb706
Salmo <salmo.cruz@gmail.com>
2022-12-28 00:54:13 -0300

    Update recoverKey.js

--- BlockchainCryptography/hash-message/recoverKey.js
+++ BlockchainCryptography/hash-message/recoverKey.js
@@ -1,3 +1,9 @@
+/* 
+1. Given a message, signature, and recoveryBit find the public key and return it! Be sure to hash the message when passing it to the recovery method.
+
+Use the noble-secp256k1 documentation to find the correct method and parameters for this one.
+*/
+
 const secp = require("ethereum-cryptography/secp256k1");
 const hashMessage = require("./hashMessage");
 
