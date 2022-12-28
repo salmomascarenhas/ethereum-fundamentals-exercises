cb438b71dc301ac554efc8eb0bd90c2d3d2ef23f
Salmo <salmo.cruz@gmail.com>
2022-12-28 00:00:50 -0300

    Update signMessage.js

--- BlockchainCryptography/hash-message/signMessage.js
+++ BlockchainCryptography/hash-message/signMessage.js
@@ -5,7 +5,7 @@ const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29
 
 async function signMessage(msg) { 
     const msgHash = hashMessage(msg); // Hash message with SHA256 Hash;     
-    const signatureMessage = await secp.sign(msgHash, PRIVATE_KEY); // Asign message with PIVATE_KEY;
+    const signatureMessage = await secp.sign(msgHash, PRIVATE_KEY, {recovered: true}); // Asign message with PIVATE_KEY;
 
     return signatureMessage;
 }
