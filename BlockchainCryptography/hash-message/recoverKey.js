5a6546f8fc366489e089ea3c8dfe47c92357ff15
Salmo <salmo.cruz@gmail.com>
2022-12-28 00:24:57 -0300

    Create recoverKey.js

---
+++ BlockchainCryptography/hash-message/recoverKey.js
@@ -0,0 +1,10 @@
+const secp = require("ethereum-cryptography/secp256k1");
+const hashMessage = require("./hashMessage");
+
+async function recoverKey(message, signature, recoveryBit) {
+    const msgHash = hashMessage(message);
+    
+    return secp.recoverPublicKey(msgHash, signature, recoveryBit);
+}
+
+module.exports = recoverKey;
